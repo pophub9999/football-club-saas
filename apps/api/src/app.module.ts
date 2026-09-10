e037945fdc5780e05660f33b891cc9725ae8ad80
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-10 12:48:34 +0100

    feat(api): wire tenancy into application

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { TenancyModule } from './tenancy/tenancy.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, HealthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, TenancyModule, HealthModule],
 })
 export class AppModule {}
