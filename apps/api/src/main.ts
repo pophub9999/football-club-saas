eda7af5dba6eb23440847f383dbd1ffb19c68390
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-10 12:24:50 +0100

    feat: bootstrap NestJS API

---
+++ apps/api/src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableShutdownHooks();
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+}
+
+void bootstrap();
