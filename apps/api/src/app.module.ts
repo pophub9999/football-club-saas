89c32d0fdeb492c519a71ad60d28131a11d8571a
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-10 17:06:32 +0100

    feat(api): register me module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { MeModule } from './me/me.module';
 import { TenancyModule } from './tenancy/tenancy.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, TenancyModule, AuthModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TenancyModule,
+    AuthModule,
+    HealthModule,
+    MeModule,
+  ],
 })
 export class AppModule {}
