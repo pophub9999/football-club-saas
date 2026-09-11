cd9445187ccab82434e4cd7c0cb735b2a3ab9d80
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-11 01:09:14 +0100

    feat(api): register member dues module

--- apps/api/src/membership/membership.module.ts
+++ apps/api/src/membership/membership.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MembershipController } from './membership.controller';
+import { DuesController } from './dues.controller';
+import { DuesService } from './dues.service';
 
 @Module({
-  controllers: [MembershipController],
+  controllers: [MembershipController, DuesController],
+  providers: [DuesService],
 })
 export class MembershipModule {}
