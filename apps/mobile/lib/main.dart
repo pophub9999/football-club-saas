82df24eaf9c13ca6de66deee2854ea307ef670e3
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-12 10:29:10 +0100

    feat(mobile): wire branded login flow into app

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'core/branding/club_branding.dart';
 import 'core/theme/app_theme.dart';
+import 'features/auth/auth_repository.dart';
+import 'features/auth/login_screen.dart';
 import 'features/home/home_screen.dart';
 
 void main() {
@@ -18,18 +21,37 @@ void main() {
   runApp(FootballClubApp(branding: branding));
 }
 
-class FootballClubApp extends StatelessWidget {
+class FootballClubApp extends StatefulWidget {
   const FootballClubApp({super.key, required this.branding});
 
   final ClubBranding branding;
 
   @override
+  State<FootballClubApp> createState() => _FootballClubAppState();
+}
+
+class _FootballClubAppState extends State<FootballClubApp> {
+  ClubBranding? _authenticatedBranding;
+  AuthSession? _session;
+
+  Future<void> _authenticated(AuthSession session, ClubBranding branding) async {
+    setState(() {
+      _session = session;
+      _authenticatedBranding = branding;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final branding = _authenticatedBranding ?? widget.branding;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: branding.name,
       theme: buildAppTheme(branding),
-      home: HomeScreen(branding: branding),
+      home: _session == null
+          ? LoginScreen(branding: branding, onAuthenticated: _authenticated)
+          : HomeScreen(branding: branding),
     );
   }
 }
