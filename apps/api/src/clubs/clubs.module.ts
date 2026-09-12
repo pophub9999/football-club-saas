13dbd09df193662ed3cd944b6ffe16b6ef617b22
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-12 10:30:05 +0100

    feat(clubs): register branding admin controller

--- apps/api/src/clubs/clubs.module.ts
+++ apps/api/src/clubs/clubs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AdminClubsController } from './admin-clubs.controller';
 import { ClubsController } from './clubs.controller';
 import { ClubsService } from './clubs.service';
 
 @Module({
-  controllers: [ClubsController],
+  controllers: [ClubsController, AdminClubsController],
   providers: [ClubsService],
   exports: [ClubsService],
 })
