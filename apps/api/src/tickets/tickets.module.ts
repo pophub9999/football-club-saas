59efbf31a9c74d9c67979c9ef3c7b99841c690bb
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-15 09:36:39 +0100

    feat(tickets): register scanner service

--- apps/api/src/tickets/tickets.module.ts
+++ apps/api/src/tickets/tickets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
+import { TicketScannerService } from './ticket-scanner.service';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService],
-  exports: [TicketsService],
+  providers: [TicketsService, TicketScannerService],
+  exports: [TicketsService, TicketScannerService],
 })
 export class TicketsModule {}
