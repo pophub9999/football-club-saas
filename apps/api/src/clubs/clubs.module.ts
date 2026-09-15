495bcb421f72578ef545a9eafb9cb6415cfc1170
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-15 16:27:28 +0100

    feat(matchday): register matchday admin controller

--- apps/api/src/clubs/clubs.module.ts
+++ apps/api/src/clubs/clubs.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminClubsController } from './admin-clubs.controller';
-import { ClubsController } from './clubs.controller';
+import { ClubsController, ClubsMatchdayAdminController } from './clubs.controller';
 import { ClubsService } from './clubs.service';
 
 @Module({
-  controllers: [ClubsController, AdminClubsController],
+  controllers: [ClubsController, AdminClubsController, ClubsMatchdayAdminController],
   providers: [ClubsService],
   exports: [ClubsService],
 })
