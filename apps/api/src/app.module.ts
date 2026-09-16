280f72e1713d2d6d0ffc5685e3f26bde2a9162a0
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 21:57:34 +0100

    feat(api): register rewards module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,9 +7,10 @@ import { FootballModule } from './football/football.module';
 import { ContentModule } from './content/content.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { RewardsModule } from './rewards/rewards.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, MembersModule, BillingModule, FootballModule, ContentModule, TicketsModule, NotificationsModule, AdminModule],
+  imports: [PrismaModule, AuthModule, MembersModule, BillingModule, FootballModule, ContentModule, TicketsModule, NotificationsModule, RewardsModule, AdminModule],
 })
 export class AppModule {}
