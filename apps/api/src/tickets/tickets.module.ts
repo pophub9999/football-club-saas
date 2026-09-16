84d2ebc53a189ca3f8e75e33811e9a5509d1a15d
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 12:12:03 +0100

    feat(tickets): register transfer inbox controller

--- apps/api/src/tickets/tickets.module.ts
+++ apps/api/src/tickets/tickets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
+import { TicketTransfersController } from './ticket-transfers.controller';
 import { TicketsService } from './tickets.service';
 import { TicketScannerService } from './ticket-scanner.service';
 
 @Module({
-  controllers: [TicketsController],
+  controllers: [TicketsController, TicketTransfersController],
   providers: [TicketsService, TicketScannerService],
   exports: [TicketsService, TicketScannerService],
 })
