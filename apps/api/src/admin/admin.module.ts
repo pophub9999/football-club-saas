c93d12ba756c2b35fdcaace651afcda35204e759
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 21:52:54 +0100

    feat(admin): register club management

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -4,10 +4,12 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AdminBrandingController } from './admin-branding.controller';
 import { AdminBrandingService } from './admin-branding.service';
+import { AdminClubController } from './admin-club.controller';
+import { AdminClubService } from './admin-club.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AdminController, AdminBrandingController],
-  providers: [AdminService, AdminBrandingService],
+  controllers: [AdminController, AdminBrandingController, AdminClubController],
+  providers: [AdminService, AdminBrandingService, AdminClubService],
 })
 export class AdminModule {}
