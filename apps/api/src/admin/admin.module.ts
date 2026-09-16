38cbf7ab3965b2465669f12c5883dbdda636d0c1
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 21:50:57 +0100

    feat(admin): register branding endpoints

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { AdminBrandingController } from './admin-branding.controller';
+import { AdminBrandingService } from './admin-branding.service';
 
 @Module({
-  controllers: [AdminController],
-  providers: [AdminService],
+  imports: [PrismaModule],
+  controllers: [AdminController, AdminBrandingController],
+  providers: [AdminService, AdminBrandingService],
 })
 export class AdminModule {}
