030b0c9a1fcfa33324bb2d503c32c33e92e5999f
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 21:57:30 +0100

    feat(admin): register rewards and notification management

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RewardsModule } from '../rewards/rewards.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AdminBrandingController } from './admin-branding.controller';
@@ -8,10 +10,13 @@ import { AdminClubController } from './admin-club.controller';
 import { AdminClubService } from './admin-club.service';
 import { AdminSettingsController } from './admin-settings.controller';
 import { AdminSettingsService } from './admin-settings.service';
+import { AdminRewardsController } from './admin-rewards.controller';
+import { AdminNotificationsController } from './admin-notifications.controller';
+import { AdminNotificationsService } from './admin-notifications.service';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AdminController, AdminBrandingController, AdminClubController, AdminSettingsController],
-  providers: [AdminService, AdminBrandingService, AdminClubService, AdminSettingsService],
+  imports: [PrismaModule, NotificationsModule, RewardsModule],
+  controllers: [AdminController, AdminBrandingController, AdminClubController, AdminSettingsController, AdminRewardsController, AdminNotificationsController],
+  providers: [AdminService, AdminBrandingService, AdminClubService, AdminSettingsService, AdminNotificationsService],
 })
 export class AdminModule {}
