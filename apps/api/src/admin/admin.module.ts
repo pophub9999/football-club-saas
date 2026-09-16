035a60c754b1879cbec30c89c8c6a647aefed48f
pophub9999 <ricardofrancosilva@gmail.com>
2026-09-16 21:53:07 +0100

    feat(admin): register tenant settings

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -6,10 +6,12 @@ import { AdminBrandingController } from './admin-branding.controller';
 import { AdminBrandingService } from './admin-branding.service';
 import { AdminClubController } from './admin-club.controller';
 import { AdminClubService } from './admin-club.service';
+import { AdminSettingsController } from './admin-settings.controller';
+import { AdminSettingsService } from './admin-settings.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AdminController, AdminBrandingController, AdminClubController],
-  providers: [AdminService, AdminBrandingService, AdminClubService],
+  controllers: [AdminController, AdminBrandingController, AdminClubController, AdminSettingsController],
+  providers: [AdminService, AdminBrandingService, AdminClubService, AdminSettingsService],
 })
 export class AdminModule {}
